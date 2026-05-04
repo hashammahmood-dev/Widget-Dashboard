c9210bf9ee110335e8bf2f65c647387deb676f79
hashammahmood-dev <muhammadhashammahmood@gmail.com>
2026-05-04 11:41:43 +0500

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Login from "./pages/login";
-import Signup from "./pages/signup";
+//import Login from "./pages/login";
+//import Signup from "./pages/signup";
 import Dashboard from "./pages/dashboard";
 import MyWidgets from "./pages/myWidgets";
 
@@ -9,8 +9,8 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
+        {/* <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<Signup />} /> */}
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/my-widgets" element={<MyWidgets />} />
       </Routes>
