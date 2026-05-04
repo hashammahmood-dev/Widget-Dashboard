d585fc08be7487d16f5678cd76ee00ee18a76d4d
hashammahmood-dev <muhammadhashammahmood@gmail.com>
2026-05-04 12:07:38 +0500

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,29 @@
-// import { BrowserRouter, Routes, Route } from "react-router-dom";
+// // import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-// //import Login from "./pages/login";
-// //import Signup from "./pages/signup";
+// // //import Login from "./pages/login";
+// // //import Signup from "./pages/signup";
+// // import Dashboard from "./pages/dashboard";
+// // import MyWidgets from "./pages/myWidgets";
+
+// // function App() {
+// //   return (
+// //     <BrowserRouter>
+// //       <Routes>
+         
+// //         {/* <Route path="/" element={<Login />} />
+// //         <Route path="/signup" element={<Signup />} /> */}
+// //         <Route path="/dashboard" element={<Dashboard />} />
+// //         <Route path="/my-widgets" element={<MyWidgets />} />
+       
+// //       </Routes>
+// //     </BrowserRouter>
+// //   );
+// // }
+
+// // export default App;
+
+// import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
 // import Dashboard from "./pages/dashboard";
 // import MyWidgets from "./pages/myWidgets";
 
@@ -9,34 +31,38 @@
 //   return (
 //     <BrowserRouter>
 //       <Routes>
-         
-//         {/* <Route path="/" element={<Login />} />
-//         <Route path="/signup" element={<Signup />} /> */}
+
+//         {/* Redirect root to dashboard */}
+//         <Route path="/" element={<Navigate to="/dashboard" />} />
+
 //         <Route path="/dashboard" element={<Dashboard />} />
 //         <Route path="/my-widgets" element={<MyWidgets />} />
-       
+//         <Route path="/login" element={<login />} />
+
 //       </Routes>
 //     </BrowserRouter>
 //   );
 // }
 
 // export default App;
-
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 import Dashboard from "./pages/dashboard";
 import MyWidgets from "./pages/myWidgets";
+import Login from "./pages/login";   // 👈 add back
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
 
-        {/* Redirect root to dashboard */}
+        {/* Default → Dashboard */}
         <Route path="/" element={<Navigate to="/dashboard" />} />
 
+        {/* Pages */}
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/my-widgets" element={<MyWidgets />} />
+        <Route path="/login" element={<Login />} />  {/* 👈 needed */}
 
       </Routes>
     </BrowserRouter>
